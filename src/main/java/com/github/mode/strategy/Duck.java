315fa975bdf1ae5cacfbcfa008bc7f9b53ba4693
wuchuan <609833577@qq.com>
2023-03-07 18:32:13 +0800

    策略模式

--- src/main/java/com/github/mode/strategy/Duck.java
+++ src/main/java/com/github/mode/strategy/Duck.java
@@ -1,5 +1,24 @@
 package com.github.mode.strategy;
 
 // 策略模式
-public class Duck {
+public abstract class Duck {
+  QuackBehavior quackBehavior;
+  FlyBehavior flyBehavior;
+
+  public Duck() {
+  }
+
+  public abstract void display();
+
+  public void performFly() {
+    flyBehavior.fly();
+  }
+
+  public void performQuack() {
+    quackBehavior.quack();
+  }
+
+  public void swim() {
+    System.out.println("All ducks float, even decoys!");
+  }
 }
---
+++ src/main/java/com/github/mode/strategy/Quack.java
@@ -0,0 +1,8 @@
+package com.github.mode.strategy;
+
+public class Quack implements QuackBehavior{
+  @Override
+  public void quack() {
+
+  }
+}
